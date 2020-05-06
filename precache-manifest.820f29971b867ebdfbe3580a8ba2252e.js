bdd27d9d4afe0a45f0b620005ba4631f2c5e492c
侯欣榆 <xinyu.hou@dhc.com.cn>
2020-05-06 16:10:59 +0800

    重新打包v-3

---
+++ precache-manifest.820f29971b867ebdfbe3580a8ba2252e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "00501572e0802f0436c6",
+    "url": "./static/css/main.8dbc175d.chunk.css"
+  },
+  {
+    "revision": "00501572e0802f0436c6",
+    "url": "./static/js/main.00501572.chunk.js"
+  },
+  {
+    "revision": "16cfa053861dad06ef80",
+    "url": "./static/css/1.6dfb1d4c.chunk.css"
+  },
+  {
+    "revision": "16cfa053861dad06ef80",
+    "url": "./static/js/1.16cfa053.chunk.js"
+  },
+  {
+    "revision": "42ab451dab28e6b0bdda",
+    "url": "./static/css/2.f4c6bad9.chunk.css"
+  },
+  {
+    "revision": "42ab451dab28e6b0bdda",
+    "url": "./static/js/2.42ab451d.chunk.js"
+  },
+  {
+    "revision": "3c1ef93f5d9f06ad12b5",
+    "url": "./static/css/3.5a93534e.chunk.css"
+  },
+  {
+    "revision": "3c1ef93f5d9f06ad12b5",
+    "url": "./static/js/3.3c1ef93f.chunk.js"
+  },
+  {
+    "revision": "cde4cd60b08a1d554917",
+    "url": "./static/js/runtime~main.cde4cd60.js"
+  },
+  {
+    "revision": "09fc9d453bf49756c734d9c8db2e3217",
+    "url": "./static/media/site-header-logo.09fc9d45.png"
+  },
+  {
+    "revision": "a2bf77d2347b0d88b7adbcf925739b9a",
+    "url": "./static/media/download.a2bf77d2.jpg"
+  },
+  {
+    "revision": "b009f4f5fc8924d5cc62d618508b5a09",
+    "url": "./static/media/one.b009f4f5.png"
+  },
+  {
+    "revision": "58cf9a72f9f81bd6681949f175bba5b5",
+    "url": "./static/media/brief-1.58cf9a72.png"
+  },
+  {
+    "revision": "bc018ba007b534f41412d11023edfcff",
+    "url": "./static/media/brief-2.bc018ba0.png"
+  },
+  {
+    "revision": "8562c5bccb672cf3aca65ea9bad3603c",
+    "url": "./static/media/brief-3.8562c5bc.png"
+  },
+  {
+    "revision": "8bc2703d96aed35a599a74178845d3ac",
+    "url": "./static/media/qr.8bc2703d.jpg"
+  },
+  {
+    "revision": "20a7805607de91e3b8213ec8e61ddc3f",
+    "url": "./index.html"
+  }
+];
